4eec10a4f58e2a913ea539000ddf556c643d915e
David Domene Domene <daviddomene97@gmail.com>
2019-09-25 13:03:49 +0200

    ok

---
+++ PruebaHilosPractica/src/pruebahilospractica/Observer.java
@@ -0,0 +1,14 @@
+package pruebahilospractica;
+
+/**
+ *
+ * @author David Domene Domene
+ */
+public class Observer {
+    public static void tiempoEjecucion(){
+        
+        
+    
+    }
+
+}
